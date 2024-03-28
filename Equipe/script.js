3766cde06fa0377b1270178944575a0ef6625226
Furbim <155479087+Furbim@users.noreply.github.com>
2024-03-28 15:47:35 -0300

    Update script.js

--- Equipe/script.js
+++ Equipe/script.js
@@ -30,56 +30,56 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
     mudar('.q2', 'f');
   }
 
-  if (answers.q2 && answers.q2.value === 'd') {
+  if (answers.q3 && answers.q3.value === 'd') {
     score++;
     mudar('.q3', 't');
   } else {
     mudar('.q3', 'f');
   }
 
-  if (answers.q2 && answers.q2.value === 'b') {
+  if (answers.q4 && answers.q4.value === 'b') {
     score++;
     mudar('.q4', 't');
   } else {
     mudar('.q4', 'f');
   }
 
-  if (answers.q2 && answers.q2.value === 'b') {
+  if (answers.q5 && answers.q5.value === 'b') {
     score++;
     mudar('.q5', 't');
   } else {
     mudar('.q5', 'f');
   }
 
-  if (answers.q2 && answers.q2.value === 'c') {
+  if (answers.q6 && answers.q6.value === 'c') {
     score++;
     mudar('.q6', 't');
   } else {
     mudar('.q6', 'f');
   }
 
-  if (answers.q2 && answers.q2.value === 'b') {
+  if (answers.q7 && answers.q7.value === 'b') {
     score++;
     mudar('.q7', 't');
   } else {
     mudar('.q7', 'f');
   }
 
-  if (answers.q2 && answers.q2.value === 'b') {
+  if (answers.q8 && answers.q8.value === 'b') {
     score++;
     mudar('.q8', 't');
   } else {
     mudar('.q8', 'f');
   }
 
-  if (answers.q2 && answers.q2.value === 'a') {
+  if (answers.q9 && answers.q9.value === 'a') {
     score++;
     mudar('.q9', 't');
   } else {
     mudar('.q9', 'f');
   }
 
-  if (answers.q2 && answers.q2.value === 'a') {
+  if (answers.q10 && answers.q10.value === 'a') {
     score++;
     mudar('.q10', 't');
   } else {
